fddc7215f7df4b6a095d61ff2cff1dd2c7795806
Audrey Guigo <guigo.audrey@gmail.com>
2023-02-24 15:17:44 +0100

    watch list day 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "lists#index"
-  resources :lists, except: [:edit, :update, :destroy] do
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
